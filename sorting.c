38d7ef4115629089245e790f3414844977e231d7
Frnot <frnot@frnot.com>
2023-05-21 15:47:28 -0400

    implemented some (currently broken) sorting logic

--- sorting.c
+++ sorting.c
@@ -1,15 +1,21 @@
-#include <iostream>
-using namespace std;
-
+#include <stdio.h>
+#include <string.h>
 
 // an algorithm that attempts sort an array arbitrarily using the least number of move operations
 // for this problem, moving an array element from any index n to m is considered a single operation
 // other measures of time complexity are not considered
 
 // This algorithm was developed primarily to sort a youtube playlist using the least number of API calls
-// ported to c++ just because
+// ported to C just because
 
 int main(){
+
+    const int size = 10;
+
+    int order[size] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int data[size] = {8, 7, 3, 5, 9, 4, 0, 6, 2, 1};
+
+
 
     return 0;
 }
--- sorting.py
+++ sorting.py
@@ -15,9 +15,19 @@ def main():
     order = data.copy()
     random.shuffle(data)
 
+    data = [7, 3, 1, 0, 8, 2, 5, 9, 4, 6]
+
     print(f"{order=}")
     print(f"{data=}")
 
+    
+
+    biog = sort(data, order) 
+
+
+
+
+def sort(data, order):
     groups = []
     traversed = set()
 
@@ -29,9 +39,78 @@ def main():
 
     for group in groups:
         if len(group) > len(biog):
-            biog = group  
+            biog = group
+
+    # find the final indices these elements will have:
+    biog_order = []
+    for element in biog:
+        biog_order.append(order.index(element))
 
     print(f"{biog=}")
+    print(f"{biog_order=}")
+
+
+    # the elements in biog are already sorted so they will not move
+    # all the other elements needed to be sorted accordingly
+
+    # find indices of nonsorted elements
+    unsorted_indices = []
+    for idx,element in enumerate(data):
+        if element not in biog:
+            unsorted_indices.append(idx)
+
+    print(f"{unsorted_indices=}")
+
+
+    ###### TODO:
+    # everytime un unsorted list is sorted, a new list of sorted items needs to be generated
+    # and the list of unsorted indices needs to be updated
+
+    # sort the unsorted elements, keeping a record of operations performed
+    sorting_ops = []
+    for i in unsorted_indices:
+        # translate the order for this element
+        # order index is where the current unsorted index (i) is located in the final sorted list
+        order_idx = order.index(data[i])
+
+        # find where this element should be moved relatived to the sorted group
+        if order_idx < biog_order[0]: # special case for beginning of list
+            move(sorting_ops,data,i,0,unsorted_indices)
+        else:
+            prev = biog_order[0]
+            for s in biog_order[1:]:
+                next = s
+                if order_idx < next:
+                    new_idx = data.index(prev)
+                    move(sorting_ops,data,i,new_idx,unsorted_indices)
+                    break
+                prev = next
+            else: # special case for end of list
+                new_idx = data.index(next)+1
+                move(sorting_ops,data,i,new_idx,unsorted_indices)
+            
+        print()
+        print(f"{sorting_ops=}")
+        print(f"{data=}")
+        print(f"{unsorted_indices=}")
+
+    #TODO regenerate out of order indices every time we make a move
+
+    print(f"{data=}")
+    print(f"{sorting_ops=}")
+            
+        
+
+def move(sorting_ops, data, old_idx, new_idx, unsorted_indices):
+    sorting_ops.append(("mov", old_idx, new_idx))
+    data.insert(new_idx, data.pop(old_idx))
+
+    for idx,u in enumerate(unsorted_indices[1:],start=1):
+        if unsorted_indices[0] < u:
+            unsorted_indices[idx] = u-1
+
+
+
 
 
 def biggest_inorder_group(partial_list):
